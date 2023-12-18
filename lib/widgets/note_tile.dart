41c32040152cbeb627937d6d60046841e6f4f484
Kaleab Solomon <kaleabslmn@gmail.com>
2023-12-18 10:31:05 +0300

    basic layout for note_tile

--- lib/widgets/note_tile.dart
+++ lib/widgets/note_tile.dart
@@ -8,17 +8,26 @@ class NoteTile extends StatefulWidget {
 }
 
 class _NoteTileState extends State<NoteTile> {
+  DateTime currentDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(8),
-      width: MediaQuery.of(context).size.width * 0.4,
-      height: 100,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-        borderRadius: BorderRadius.circular(14),
-      ),
-      child: Text("hello world"),
-    );
+        margin: EdgeInsets.all(8),
+        width: MediaQuery.of(context).size.width * 0.4,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          borderRadius: BorderRadius.circular(14),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("note title"),
+            Text("note content"),
+            Text('${currentDate.year}-${currentDate.month}-${currentDate.day}')
+          ],
+        ));
   }
 }
