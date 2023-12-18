fed638b1020a5d33af02a4abd3cbff0307bc1ab6
Kaleab Solomon <kaleabslmn@gmail.com>
2023-12-18 15:07:19 +0300

    make title and content required

--- lib/pages/add_note_page.dart
+++ lib/pages/add_note_page.dart
@@ -14,11 +14,16 @@ class _AddNotePageState extends State<AddNotePage> {
   //FIXME: light-dark mode icons not interchanging
   bool isDarkMode = false;
 
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
   void toggleTheme() {
     Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
   }
 
   List<Widget> _appBarActions = [];
+  bool isTitleFieldEmpty = false;
+  bool isContentFieldEmpty = false;
 
   @override
   void didChangeDependencies() {
@@ -26,7 +31,26 @@ class _AddNotePageState extends State<AddNotePage> {
     // initialize app bar actions
     _appBarActions = [
       IconButton(
-        onPressed: () {},
+        onPressed: () {
+          _titleController.text.isEmpty
+              ? setState(() {
+                  isTitleFieldEmpty = true;
+                })
+              : setState(() {
+                  isTitleFieldEmpty = false;
+                });
+          _contentController.text.isEmpty
+              ? setState(() {
+                  isContentFieldEmpty = true;
+                })
+              : setState(() {
+                  isContentFieldEmpty = false;
+                });
+
+          print(
+              'title: ${_titleController.text.trim()} and content:${_contentController.text.trim()}');
+          // save notes to hive
+        },
         icon: Icon(
             color: Theme.of(context).colorScheme.primary, size: 30, Icons.save),
       ),
@@ -49,13 +73,15 @@ class _AddNotePageState extends State<AddNotePage> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               TextField(
+                controller: _titleController,
                 style: TextStyle(
                     fontSize: 24, color: Theme.of(context).colorScheme.primary),
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 10),
                   // error: Text("Title is required"),
                   hintText: 'Enter Title Here',
-                  hintStyle: TextStyle(
+                  errorText: isTitleFieldEmpty ? "a title is required" : null,
+                  hintStyle: const TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.w400,
                   ),
@@ -66,14 +92,17 @@ class _AddNotePageState extends State<AddNotePage> {
               ),
               Expanded(
                   child: TextField(
+                controller: _contentController,
                 style: TextStyle(
                     color: Theme.of(context).colorScheme.primary, fontSize: 18),
                 maxLines: null,
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                  errorText:
+                      isTitleFieldEmpty ? "A note should have a content" : null,
                   hintText: "Enter Content Here",
-                  hintStyle: TextStyle(
+                  hintStyle: const TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w400,
                   ),
